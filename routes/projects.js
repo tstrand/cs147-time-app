388ebd4b8a1e3c2d4746834f9d5373e6ecabd920
Tyler M. Strand <tyler@hostess.fm>
2014-02-11 21:58:43 -0800

    headers, data, project pages

--- routes/projects.js
+++ routes/projects.js
@@ -4,8 +4,19 @@ exports.viewProjects = function(req, res) { 
 	var projectId = req.params.projectId; 
 	console.log(projectId);
 	if(projectId) {
-		res.render('project', {"projectId": projectId});
+		var object;
+		for(var i = 0; i < data["projects"].length; i++) {
+			if(projectId == data["projects"][i]["id"]) {
+				object = data["projects"][i];
+				break;
+			}
+		}
+		object["pageName"] = object["name"];
+		object["tasks"] = data["tasks"];
+		object["meetings"] = data["meetings"];
+		res.render('project', object);
 	} else {
+		data["pageName"] = "My Projects";
 		res.render('projects', data);
 	}
 };
@@ -15,14 +26,14 @@ exports.newProject = function(req, res) {
 }
 
 exports.createProject = function(req, res) {    
+	var id = Math.floor(Math.random() * 1000) + 10;
 	var project = {
-		"id": Math.floor(Math.random() * 1000) + 10,
+		"id": id,
 		"name": req.query.name,
 		"dueDate": req.query.duedate,
 		"description": 	req.query.description,
 		"members": req.query.members	
 	};
-	console.log(project);
 	data["projects"].push(project);
-	res.render('projects', data);
+	res.redirect('/projects/' + id);
  }
