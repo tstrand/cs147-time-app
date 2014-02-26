4fb88a80c3d555454d64cdbd1990e817cafbd4eb
Tyler M. Strand <tyler@hostess.fm>
2014-02-25 23:17:07 -0800

    agenda on DB

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-var data = require('../data.json');
 var models = require('../models');
 
 exports.viewAgenda = function(req, res) { 
@@ -7,87 +6,95 @@ exports.viewAgenda = function(req, res) { 
   } else {
     var name = req.params.name; 
 
-  // filter my tasks
-  mydata = {};
-  mydata["tasks"] = [];
-  mydata["meetings"] = [];
-  mydata["completed_tasks"] = [];
-  for (var i=0; i<data["tasks"].length; i++) {
-    for (var j=0; j<data["tasks"][i]["members"].length; j++) {
-      if ((req.session.user_id == data["tasks"][i]["members"][j] ||
-          req.session.username == data["tasks"][i]["members"][j]) &&
-          data["tasks"][i]["parent"] != -1) {
-        if(data["tasks"][i]["done"]) {
-          mydata["completed_tasks"].push(data["tasks"][i]);
-        } else {
-          mydata["tasks"].push(data["tasks"][i]);
+    // filter my tasks
+    var object = {};
+
+    models.Meetings.find().sort("datetime").exec(gotMeeting);
+
+    function gotMeeting(err, meetings) {
+      object["meetings"] = [];
+      for (var i=0; i<meetings.length; i++) {
+        for (var j=0; j<meetings[i]["members"].length; j++) {
+          if (req.session.username == meetings[i]["members"][j]) {
+            object["meetings"].push(meetings[i]);
+            break;
+          }
         }
-        break;
       }
+      models.Tasks.find().sort("dueDate").exec(gotTasks);
     }
-  }
-  for (var i=0; i<data["meetings"].length; i++) {
-    for (var j=0; j<data["meetings"][i]["members"].length; j++) {
-      if (req.session.user_id == data["meetings"][i]["members"][j] ||
-          req.session.username == data["meetings"][i]["members"][j]) {
-        var cur = new Date().getTimezoneOffset() * -1;
-        console.log(cur);
-        if(cur > data["meetings"][i]["datetime"]) { 
-          data["meetings"][i]["datetime"] = 0;
+
+    function gotTasks(err, tasks) {
+      object["tasks"] = [];
+      object["completed_tasks"] = [];
+      for (var i=0; i<tasks.length; i++) {
+        for(var j=0; j<tasks[i]["members"]; j++) {
+          if (req.session.username == tasks[i]["members"][j] && tasks[i]["parent_id"] == -1) {
+            if (tasks[i]["done"]) {
+              object["completed_tasks"].push(tasks[i]);
+            } else {
+              object["tasks"].push(tasks[i]);
+            }
+          }
         }
-        mydata["meetings"].push(data["meetings"][i]);
-        break;
       }
+      for (var i=0; i<object["tasks"].length; i++) {
+        // getting parent task
+        for (var j=0; j<tasks.length; j++) {
+          if (tasks[j]["id"] == object["tasks"][i]["parent"]) {
+            object["tasks"][i]["parent_name"] = tasks[j]["name"];
+            break;
+          }
+        }
+      }
+      for (var i=0; i<object["completed_tasks"].length; i++) {
+        // getting parent task
+        for (var j=0; j<tasks.length; j++) {
+          if (tasks[j]["id"] == object["completed_tasks"][i]["parent"]) {
+            object["completed_tasks"][i]["parent_name"] = tasks[j]["name"];
+            break;
+          }
+        }
+      }
+
+      models.Projects.find().exec(gotProject);
+    }
+
+    function gotProject(err, projects) {
+      for (var i=0; i<object["tasks"].length; i++) {
+        //getting project name
+        for (var j=0; j<projects.length; j++) {
+          if (projects[j]["id"] == object["tasks"][i]["project_id"]) {
+            object["tasks"][i]["project_name"] = projects[j]["name"];
+            break;
+          }
+        }
+      }
+
+      for (var i=0; i<object["completed_tasks"].length; i++) {
+        //getting project name
+        for (var j=0; j<projects.length; j++) {
+          if (projects[j]["id"] == object["completed_tasks"][i]["project_id"]) {
+            object["completed_tasks"][i]["project_name"] = projects[j]["name"];
+            break;
+          }
+        }
+      }
+      
+      for (var i=0; i<object["meetings"].length; i++) {
+        //getting project name
+        for (var j=0; j<projects.length; j++) {
+          if (projects[j]["id"] == object["meetings"][i]["project_id"]) {
+            object["meetings"][i]["project_name"] = projects[j]["name"];
+            break;
+          }
+        }
+      }
+
+      object["pageName"] = "My Agenda";
+      object["user_id"] = req.session.user_id;
+      object["username"] = req.session.username;
+      res.render('index', object);
     }
   }
-
-  
-  for (var i=0; i<mydata["tasks"].length; i++) {
-    // getting parent task
-    for (var j=0; j<data["tasks"].length; j++) {
-      if (data["tasks"][j]["id"] == mydata["tasks"][i]["parent"]) {
-        mydata["tasks"][i]["parent_name"] = data["tasks"][j]["name"];
-        break;
-      }
-    }
-    //getting project name
-    for (var j=0; j<data["projects"].length; j++) {
-      if (data["projects"][j]["id"] == mydata["tasks"][i]["project_id"]) {
-        mydata["tasks"][i]["project_name"] = data["projects"][j]["name"];
-      }
-    }
-  }
-
-  for (var i=0; i<mydata["completed_tasks"].length; i++) {
-    // getting parent task
-    for (var j=0; j<data["tasks"].length; j++) {
-      if (data["tasks"][j]["id"] == mydata["completed_tasks"][i]["parent"]) {
-        mydata["completed_tasks"][i]["parent_name"] = data["tasks"][j]["name"];
-        break;
-      }
-    }
-    //getting project name
-    for (var j=0; j<data["projects"].length; j++) {
-      if (data["projects"][j]["id"] == mydata["completed_tasks"][i]["project_id"]) {
-        mydata["completed_tasks"][i]["project_name"] = data["projects"][j]["name"];
-      }
-    }
-  }
-
-  for (var i=0; i<mydata["meetings"].length; i++) {
-    //getting project name
-    for (var j=0; j<data["projects"].length; j++) {
-      if (data["projects"][j]["id"] == mydata["meetings"][i]["project_id"]) {
-        mydata["meetings"][i]["project_name"] = data["projects"][j]["name"];
-      }
-    }
-  }
-
-  mydata["pageName"] = "My Agenda";
-  mydata["user_id"] = req.session.user_id;
-  mydata["username"] = req.session.username;
-  //res.render('agenda', mydata);
-    res.render('index', mydata);
-  }
-
 };
